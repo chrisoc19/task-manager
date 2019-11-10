ab57d7d24041941d87d419530f1c4452e884e1d2
chrisoc19 <chrisoconnor904@yahoo.com.au>
2019-11-10 14:17:04 +0000

    added ad task functionality

--- app.py
+++ app.py
@@ -18,7 +18,13 @@ def get_tasks():
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
